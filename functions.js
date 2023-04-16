c6f1331e576e16f3bb33a7abeecc9e9b0e7fa62e
italoleosantos <italo.lima.santos@hotmail.com>
2023-04-15 22:05:56 -0300

    JS

--- functions.js
+++ functions.js
@@ -1,3 +1,55 @@
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function mdc(a,b){
+  if (b==0){
+    return a;
+  }
+  return mdc(b, a % b)
+}
+
+function gerar_chaves(p,q){
+  let n = p * q;
+  let func_tot = (p-1) * (q-1)
+
+  while (true) {
+    let e = Math.floor(Math.random() * (phi - 2 + 1) + 2);
+    if (gcd(e, phi) === 1) {
+      break;
+    }
+  }
+  
+  let d = 0;
+  for (let i = 1; i < phi; i++) {
+    if ((i * e) % phi === 1) {
+      d = i;
+      break;
+    }
+  }
+  
+  return [[e, n], [d, n]];
+
+}
+
+
+
+
+
+
+
+
+
+
+/*
 function verificarPrimos() {
   const p = parseInt(document.getElementById("p-input").value);
   const q = parseInt(document.getElementById("q-input").value);
@@ -30,3 +82,4 @@ function MostrarCampos() {
   document.getElementById("campo1").style.display = "block";
   document.getElementById("campo2").style.display = "block";
 }
+*/
